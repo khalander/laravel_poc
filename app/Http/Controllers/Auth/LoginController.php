c5512d295aeab8cd4ad627c3ae2f2e7669249407
Dada Khalender M <ce551@ce-551.cssad.compassitesinc.com>
2020-09-07 20:22:31 +0530

    socialite implementation

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Socialite;
 class LoginController extends Controller
 {
     /*
@@ -35,5 +35,32 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('google')->user();
+        /* 
+            1) Store the user details if not exists else just redirect
+            2) if Auth is using then login using Auth::login($user)
+            3) for passport, store some standard passport for all users
+            4) For multiple socialite option, make param route and replace everywhere option is hard coded
+        */
+        return $user->name;
     }
 }
